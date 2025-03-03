5e66d776a3dd53ab355bf58dc74c629243ade749
moulmark <mcorrochanoarroyo@gmail.com>
2025-03-03 22:12:34 +0100

    feat: new layout

---
+++ src/data/index.ts
@@ -0,0 +1,14 @@
+export const socials = [
+  {
+    url: 'https://github.com/moulmark',
+    icon: 'github-line'
+  },
+  {
+    url: 'https://x.com/moulmarkdev',
+    icon: 'twitter-x-line'
+  },
+  {
+    url: 'https://x.com/moulmarkdev',
+    icon: 'linkedin-box-line'
+  }
+]
