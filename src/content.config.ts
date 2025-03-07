fe305fae55e990a3cc681c71ed36be6238fd22e0
moulmark <mcorrochanoarroyo@gmail.com>
2025-03-07 21:21:28 +0100

    feat: blog post layout

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,12 +1,14 @@
-import { defineCollection, z } from 'astro:content'
+import { z, defineCollection } from 'astro:content'
 import { glob } from 'astro/loaders'
 
 const blog = defineCollection({
-  loader: glob({ pattern: '**/*.md', base: './src/posts' }),
+  loader: glob({ pattern: '**/*.{md,mdx}', base: './src/posts' }),
   schema: z.object({
     title: z.string(),
-    description: z.string(),
-    pubDate: z.coerce.date()
+    description: z.string().min(10).max(100),
+    pubDate: z.date(),
+    author: z.string(),
+    image: z.string().optional()
   })
 })
 
--- src/posts/welcome.md
+++
@@ -1,7 +0,0 @@
----
-title: Welcome
-description: My first post in the blog!
-pubDate: 2025-03-03
----
-
-This is a test
